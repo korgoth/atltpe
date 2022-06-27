7be0876963c4f7236d40175ff3e4e16f23010cca
Emiliyan Parvanov @ M1 <work@emiliyan.com>
2022-06-27 11:02:32 +0300

    Added: validate_fields function in cart.js

--- assets/js/cart.js
+++ assets/js/cart.js
@@ -112,6 +112,71 @@ $(function(){
 		}
 	});
 
+	$('.validate_fields').click(function(e){
+		e.preventDefault();
+
+		var fields_invalid = false;
+
+		$('.order_data.required').each(function(){
+			if ($(this).val().length < 3) {
+				$(this).addClass('alert-danger');
+				if ($(this).is(':visible')) {
+					$(this).tooltipster('show');
+				}
+				fields_invalid = true;
+			} else {
+				if ($(this).is('[name=email]')) {
+					var filter = /^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([ \t]*\r\n)?[ \t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([ \t]*\r\n)?[ \t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i;
+					if (!filter.test($(this).val().trim())) {
+						$(this).addClass('alert-danger');
+						$(this).tooltipster('show');
+						fields_invalid = true;
+					} else {
+						$(this).removeClass('alert-danger');
+						$(this).tooltipster('hide');
+					}
+				} else if ($(this).is('[name=phone]')) {
+					var filter = /^(?=.*\d)[\d ]+$/;
+					if (!filter.test($(this).val().trim())) {
+						$(this).tooltipster('show');
+						$(this).addClass('alert-danger');
+						fields_invalid = true;
+					} else {
+						$(this).tooltipster('hide');
+						$(this).removeClass('alert-danger');
+					}
+				} else {
+					$(this).removeClass('alert-danger');
+					$(this).tooltipster('hide');
+				}
+			}
+		});
+
+		var btn = $(this);
+
+
+		if (!fields_invalid) {
+			
+			//check if phone is valid here
+			$.getJSON("//lib2.qckcdn.com/phone.php?p="+$('input[name=phone]:last').val()+"&c="+cc, function(data){}).done(function(data){
+				if (data.status != "OK") {
+					//phone number is invalid
+					$('input[name=phone]').tooltipster('show');
+					$('input[name=phone]').addClass('alert-danger');
+					fields_invalid = true;
+				} else {
+					//if there is a vallable specified - try to call it
+					var callable = btn.attr('data-callable');
+					if (callable) {
+						window[callable]();
+					}
+				}
+			});
+
+		}
+	});
+
+
 	$('#save_details').click(function(e){
 		e.preventDefault();
 		var btn = $(this);
